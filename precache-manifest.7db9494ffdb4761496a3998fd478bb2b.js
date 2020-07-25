f3041f61b1d279a61dcaff648e4dc2144d45c739
amylzting <amylzting@gmail.com>
2020-07-26 02:59:19 +0800

    Updates

---
+++ precache-manifest.7db9494ffdb4761496a3998fd478bb2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8abe09609fe5e2407774d30a67d490e",
+    "url": "/react-app-messenger/index.html"
+  },
+  {
+    "revision": "bd97485e2665cf57a0aa",
+    "url": "/react-app-messenger/static/css/main.1165a6f3.chunk.css"
+  },
+  {
+    "revision": "a1bfe97160e6cbedcde8",
+    "url": "/react-app-messenger/static/js/2.2db880e0.chunk.js"
+  },
+  {
+    "revision": "cfcdebbd5c361eb80dad4bc286f1bc4f",
+    "url": "/react-app-messenger/static/js/2.2db880e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd97485e2665cf57a0aa",
+    "url": "/react-app-messenger/static/js/main.be62ec99.chunk.js"
+  },
+  {
+    "revision": "bab17fe4384cc88c4ed7",
+    "url": "/react-app-messenger/static/js/runtime-main.db84f609.js"
+  }
+]);
